e3655eccb4f7ac099c85e96e0c4e95656f7d10bd
hyunsoo Kim <hyuns9411@gmail.com>
2021-04-30 13:48:57 +0900

    프런트 단 js 만들기

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs"); // view engine을 ejs로 쓴다.
 
 const home = require("./src/routes/home");
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 // use -> 미들 웨어를 등록해주는 메서드  home에 있는 파일을 쓸 수 있게 해준다.
 
