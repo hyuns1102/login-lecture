6d25f4a30ed186c5f2badb676a2d37d4c19d30e5
hyunsoo Kim <hyuns9411@gmail.com>
2021-04-30 12:57:10 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+// app setting
+app.set("views", "./src/views");
+app.set("view engine", "ejs"); // view engine을 ejs로 쓴다.
+
+const home = require("./src/routes/home");
+app.use("/", home);
+// use -> 미들 웨어를 등록해주는 메서드  home에 있는 파일을 쓸 수 있게 해준다.
+
+module.exports = app; //app을 밖에서 사용 가능하도록 내보내준다.
